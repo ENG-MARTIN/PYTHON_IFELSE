0a200645c217b2ae3afa6c162a99306fe143dc20
Martin Ssebakumba <108685258+ENG-MARTIN@users.noreply.github.com>
2023-08-29 11:55:47 +0300

    Add files via upload

---
+++ ODD AND EVEN.py
@@ -0,0 +1,16 @@
+#method 1 : for even numbers
+print('\n These are even numbers method 1')
+num=0
+for num in range(20):
+    if num%2==0:
+        print(num) 
+
+#method 2 : for even numbers
+print('\n These are even numbers : method 2')
+for num in range(0,20,2):
+        print(num)
+
+        #odd numbers
+print('\n These are odd numbers')
+for x in range(1,20,2):
+    print(x)
